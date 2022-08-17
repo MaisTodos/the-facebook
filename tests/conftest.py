84e55685d202e4b241e2590265dfc90d515a8dc4
russianbb <bruno.russian.i@gmail.com>
2022-08-17 10:41:57 -0400

    Add dynamodb_setup fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,17 @@ import pytest
 from pytest_chalice.handlers import RequestHandler
 
 from thefacebook.app import app as chalice_app
+from thefacebook.dynamodb_create_tables import (
+    dynamodb_create_tables,
+    dynamodb_delete_tables,
+)
+
+
+@pytest.fixture()
+def dynamo_setup():
+    dynamodb_create_tables()
+    yield
+    dynamodb_delete_tables()
 
 
 @pytest.fixture()
