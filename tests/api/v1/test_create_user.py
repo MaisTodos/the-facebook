906188e9d873abea5416c1fa06d86d49b4133e35
russianbb <bruno.russian.i@gmail.com>
2022-08-18 23:02:28 -0400

    Refatorando test_create_user para usar conftest de sessão

--- tests/api/v1/test_create_user.py
+++ tests/api/v1/test_create_user.py
@@ -1,21 +1,29 @@
-# criar teste de sucesso com 2 imagens, com mais de 1 rosto e sem nenhum rosto
 import base64
 import json
 
-import pytest
 from chalicelib.models import UserFaces
 
 
-@pytest.mark.parametrize(
-    "image_file_name",
-    ("tests/_image_without_faces.png", "tests/_image_many_peoples.png"),
-)
-def test_create_user_failure(api_client, dynamo_setup, image_file_name):
-    with open(image_file_name, "rb") as image_file:
-        encoded_string = base64.b64encode(image_file.read())
-        image_base_64 = encoded_string.decode()
+def test_create_user_fail_without_faces(
+    api_client, dynamo_setup, image_without_faces_base64
+):
+    payload = {"images_base64": [image_without_faces_base64]}
+    response = api_client.post(
+        "/v1/user",
+        headers={"Content-Type": "application/json"},
+        body=json.dumps(payload),
+    )
 
-    payload = {"images_base64": [image_base_64]}
+    assert response.status_code == 400
+    assert response.json == {
+        "images_base64": ["Deve conter apenas uma pessoa na imagem."]
+    }
+
+
+def test_create_user_fail_with_many_faces(
+    api_client, dynamo_setup, image_with_many_faces_base64
+):
+    payload = {"images_base64": [image_with_many_faces_base64]}
     response = api_client.post(
         "/v1/user",
         headers={"Content-Type": "application/json"},
@@ -31,12 +39,13 @@ def test_create_user_failure(api_client, dynamo_setup, image_file_name):
 def test_create_user_successfully(api_client, dynamo_setup):
     images = ["tests/_vin_diesel.jpg", "tests/_dominic_toretto.jpg"]
     images_base64 = []
+
     for image_file_name in images:
         with open(image_file_name, "rb") as image_file:
             encoded_string = base64.b64encode(image_file.read())
             images_base64.append(encoded_string.decode())
-
     payload = {"images_base64": images_base64}
+
     response = api_client.post(
         "/v1/user",
         headers={"Content-Type": "application/json"},
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,4 @@
+import base64
 import uuid
 
 import face_recognition
@@ -34,6 +35,26 @@ def known_vin_diesel_face():
 def known_dominic_toretto_face():
     image_file = face_recognition.load_image_file("tests/_dominic_toretto.jpg")
     return face_recognition.face_encodings(image_file)[0].tolist()
+
+
+@pytest.fixture(scope="session")
+def image_without_faces_base64():
+    filename = "tests/_image_without_faces.png"
+    with open(filename, "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read())
+        image_base64 = encoded_string.decode()
+
+    return image_base64
+
+
+@pytest.fixture(scope="session")
+def image_with_many_faces_base64():
+    filename = "tests/_image_with_many_faces.png"
+    with open(filename, "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read())
+        image_base64 = encoded_string.decode()
+
+    return image_base64
 
 
 @pytest.fixture(scope="function")
