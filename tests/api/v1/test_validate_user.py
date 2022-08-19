47666c38bcce9b766b77c7010aea48907c05bf33
russianbb <bruno.russian.i@gmail.com>
2022-08-18 23:10:36 -0400

    Refatorando test_validate_user para usar conftest de sessão

--- tests/api/v1/test_validate_user.py
+++ tests/api/v1/test_validate_user.py
@@ -2,21 +2,13 @@ import base64
 import json
 import uuid
 
-import pytest
 from chalicelib.models import UserFaces
 
 
-@pytest.mark.parametrize(
-    "image_file_name",
-    ("tests/_image_without_faces.png", "tests/_image_many_peoples.png"),
-)
-def test_validate_user_failure(image_file_name, api_client, dynamo_setup, vin_diesel):
-    with open(image_file_name, "rb") as image_file:
-        encoded_string = base64.b64encode(image_file.read())
-        image_base_64 = encoded_string.decode()
-
-    payload = {"user_id": vin_diesel.id, "image_base64": image_base_64}
-
+def test_validate_user_fail_without_faces(
+    api_client, dynamo_setup, vin_diesel, image_without_faces_base64
+):
+    payload = {"user_id": vin_diesel.id, "image_base64": image_without_faces_base64}
     response = api_client.post(
         "/v1/user/validate",
         headers={"Content-Type": "application/json"},
@@ -27,6 +19,55 @@ def test_validate_user_failure(image_file_name, api_client, dynamo_setup, vin_di
     assert response.json == {
         "image_base64": ["Deve conter apenas uma pessoa na imagem."]
     }
+
+
+def test_validate_user_fail_with_many_faces(
+    api_client, dynamo_setup, vin_diesel, image_with_many_faces_base64
+):
+    payload = {"user_id": vin_diesel.id, "image_base64": image_with_many_faces_base64}
+    response = api_client.post(
+        "/v1/user/validate",
+        headers={"Content-Type": "application/json"},
+        body=json.dumps(payload),
+    )
+
+    assert response.status_code == 400
+    assert response.json == {
+        "image_base64": ["Deve conter apenas uma pessoa na imagem."]
+    }
+
+
+def test_validate_user_fail_not_found(api_client, dynamo_setup):
+    payload = {
+        "user_id": str(uuid.uuid4()),
+        "image_base64": "some_fake_base64",
+    }
+
+    response = api_client.post(
+        "/v1/user/validate",
+        headers={"Content-Type": "application/json"},
+        body=json.dumps(payload),
+    )
+
+    assert response.status_code == 400
+    assert response.json == {"user_id": ["Usuário não encontrado."]}
+
+
+def test_validate_user_fail_payload_empty(api_client, dynamo_setup):
+    payload = {}
+
+    response = api_client.post(
+        "/v1/user/validate",
+        headers={"Content-Type": "application/json"},
+        body=json.dumps(payload),
+    )
+
+    expected_json = {
+        "image_base64": ["Missing data for required field."],
+        "user_id": ["Missing data for required field."],
+    }
+    assert response.status_code == 400
+    assert response.json == expected_json
 
 
 def test_validate_user_successfully_default_cutoff(
@@ -87,36 +128,3 @@ def test_validate_user_successfully_custom_cutoff(
 
     user_updated = UserFaces.get(str(vin_diesel.id))
     assert len(user_updated.faces) == 2
-
-
-def test_validate_user_not_found(api_client, dynamo_setup):
-    payload = {
-        "user_id": str(uuid.uuid4()),
-        "image_base64": "some_fake_base64",
-    }
-
-    response = api_client.post(
-        "/v1/user/validate",
-        headers={"Content-Type": "application/json"},
-        body=json.dumps(payload),
-    )
-
-    assert response.status_code == 400
-    assert response.json == {"user_id": ["Usuário não encontrado."]}
-
-
-def test_validate_payload_empty(api_client, dynamo_setup):
-    payload = {}
-
-    response = api_client.post(
-        "/v1/user/validate",
-        headers={"Content-Type": "application/json"},
-        body=json.dumps(payload),
-    )
-
-    expected_json = {
-        "image_base64": ["Missing data for required field."],
-        "user_id": ["Missing data for required field."],
-    }
-    assert response.status_code == 400
-    assert response.json == expected_json
