a5f63962068ca8646d1dda020dcbe4d1d1cd8495
russianbb <bruno.russian.i@gmail.com>
2022-08-18 23:14:53 -0400

    Refatorando test_vin_diesel para usar conftest de sessão

--- tests/test_vin_diesel.py
+++ tests/test_vin_diesel.py
@@ -1,4 +1,5 @@
 import face_recognition
+import numpy as np
 
 # https://github.com/ageitgey/face_recognition/blob/master/examples/face_distance.py
 
@@ -6,12 +7,11 @@ NORMAL_CUTOFF = 0.6
 STRICT_CUTOFF = 0.5
 
 
-def test_is_vin_diesel_dominic_toretto():
-    vin_diesel = face_recognition.load_image_file("tests/_vin_diesel.jpg")
-    vin_diesel_face = face_recognition.face_encodings(vin_diesel)[0]
-
-    dominic_toretto = face_recognition.load_image_file("tests/_dominic_toretto.jpg")
-    dominic_toretto_face = face_recognition.face_encodings(dominic_toretto)[0]
+def test_is_vin_diesel_dominic_toretto(
+    known_vin_diesel_face, known_dominic_toretto_face
+):
+    vin_diesel_face = np.array(known_vin_diesel_face)
+    dominic_toretto_face = np.array(known_dominic_toretto_face)
 
     known_encodings = [vin_diesel_face]
     face_distances = face_recognition.face_distance(
